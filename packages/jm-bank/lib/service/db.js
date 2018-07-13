2bb2adc5fadffdf98d2915633751d53aa56d5572
jamma <jeff@jamma.cn>
2018-07-13 14:59:18 +0800

    Op and pool

--- packages/jm-bank/lib/service/db.js
+++ packages/jm-bank/lib/service/db.js
@@ -2,11 +2,49 @@ const Sequelize = require('sequelize')
 const cls = require('continuation-local-storage')
 let namespace = cls.createNamespace('bank-namespace')
 Sequelize.useCLS(namespace)
+const Op = Sequelize.Op
+const operatorsAliases = {
+  $eq: Op.eq,
+  $ne: Op.ne,
+  $gte: Op.gte,
+  $gt: Op.gt,
+  $lte: Op.lte,
+  $lt: Op.lt,
+  $not: Op.not,
+  $in: Op.in,
+  $notIn: Op.notIn,
+  $is: Op.is,
+  $like: Op.like,
+  $notLike: Op.notLike,
+  $iLike: Op.iLike,
+  $notILike: Op.notILike,
+  $regexp: Op.regexp,
+  $notRegexp: Op.notRegexp,
+  $iRegexp: Op.iRegexp,
+  $notIRegexp: Op.notIRegexp,
+  $between: Op.between,
+  $notBetween: Op.notBetween,
+  $overlap: Op.overlap,
+  $contains: Op.contains,
+  $contained: Op.contained,
+  $adjacent: Op.adjacent,
+  $strictLeft: Op.strictLeft,
+  $strictRight: Op.strictRight,
+  $noExtendRight: Op.noExtendRight,
+  $noExtendLeft: Op.noExtendLeft,
+  $and: Op.and,
+  $or: Op.or,
+  $any: Op.any,
+  $all: Op.all,
+  $values: Op.values,
+  $col: Op.col
+}
 
 module.exports = function (opts) {
   let o = {
+    operatorsAliases,
     pool: {
-      max: 10,
+      max: 100,
       min: 0,
       idle: 30000
     },
