1b9102cbf9cc06f416ae2a5138e13275107bbcd3
jamma <jeff@jamma.cn>
2018-10-13 21:58:20 +0800

    config

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -1,6 +1,5 @@
 module.exports = {
   db: 'db',
-  table_name_prefix: 'table_name_prefix',
   gateway: 'gateway',
   disable_mq: 'disable_mq'
 }
--- config/development.js
+++ config/development.js
@@ -2,5 +2,6 @@ module.exports = {
   debug: true,
   port: 3000,
   lng: 'zh_CN',
-  db: 'mysql://root:123@localhost/bank'
+  db: 'mysql://root:123@localhost/bank',
+  gateway: 'http://gateway.test.jamma.cn'
 }
