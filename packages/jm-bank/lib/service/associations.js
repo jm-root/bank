d35a25530dac041f42a52cda935f88e502c7b494
jamma <jeff@jamma.cn>
2018-10-19 15:33:33 +0800

    表关系

--- packages/jm-bank/lib/service/associations.js
+++ packages/jm-bank/lib/service/associations.js
@@ -1,14 +1,21 @@
 module.exports = function (service, opts = {}) {
-  let self = service
-  self.user.hasMany(self.account, {foreignKey: 'userId'})
-  self.account.belongsTo(self.user, {foreignKey: 'userId'})
-  self.account.hasMany(self.balance, {foreignKey: 'accountId'})
-  self.balance.belongsTo(self.account, {foreignKey: 'accountId'})
-  self.balance.belongsTo(self.ct, {foreignKey: 'ctId'})
-  self.transfer.belongsTo(self.ct, {foreignKey: 'ctId'})
-  self.transfer.belongsTo(self.account, {as: 'fromAccount', foreignKey: 'fromAccountId'})
-  self.transfer.belongsTo(self.account, {as: 'toAccount', foreignKey: 'toAccountId'})
-  self.balance.hasMany(self.locker, {foreignKey: 'balanceId'})
-  self.locker.belongsTo(self.balance, {foreignKey: 'balanceId'})
-  self.locker.belongsTo(self.user, {foreignKey: 'fromUserId'})
+  const {account, balance, ct, locker, transfer, user} = service
+
+  account.belongsTo(user)
+  account.hasMany(balance)
+
+  balance.belongsTo(ct)
+  balance.belongsTo(account)
+  balance.hasMany(locker)
+
+  transfer.belongsTo(balance, {as: 'fromBalance'})
+  transfer.belongsTo(balance, {as: 'toBalance'})
+
+  locker.belongsTo(balance)
+  locker.belongsTo(user)
+
+  user.hasMany(account)
+  user.belongsTo(account, {as: 'defaultAccount', constraints: false})
+  user.belongsTo(account, {as: 'safeAccount', constraints: false})
+
 }
