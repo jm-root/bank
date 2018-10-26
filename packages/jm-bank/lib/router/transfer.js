17ead274db3e03bbcf0531c7ac4fadafe97ac2bf
jamma <jeff@jamma.cn>
2018-10-26 17:34:37 +0800

    - fields

--- packages/jm-bank/lib/router/transfer.js
+++ packages/jm-bank/lib/router/transfer.js
@@ -6,7 +6,6 @@ module.exports = function (service, opts) {
   const model = service.transfer
   let router = ms.router()
 
-  const fields = {exclude: ['fromUserId', 'toUserId']}
   router
     .add('/', 'get', async (opts = {}) => {
       const {data = {}} = opts
@@ -51,8 +50,6 @@ module.exports = function (service, opts) {
           toAccountId = doc.accountId
         }
       }
-
-      opts.fields || (opts.fields = fields)
 
       opts.order || (opts.order = [['id', 'DESC']])
 
